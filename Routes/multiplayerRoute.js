fcd92f36c0d979c468a335b5dfdda0309595265e
Lethiwe Mchunu <mchunulethiwe6@gmail.com>
2022-05-22 18:55:57 +0200

    mistake

--- Routes/multiplayerRoute.js
+++ Routes/multiplayerRoute.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const path = require('path')
-const mockGames = require('../MockGames')
+const mockGames = require('./MockGames')
 
 const router = express.Router()
 
 router.get('/multiplayer/random', function (request, response) {
     const id = request.query.id
-    response.send(mockGames.getGame(id))
+    const game = mockGames.
+    response.sendFile(path.join(__dirname, '../Views', 'singleplayer.html'))
   })
   
 router.get('/multiplayer/choose', function (request, response) {
