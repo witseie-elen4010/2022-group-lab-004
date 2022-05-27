aaded3d66199f83f32c590d3dd5155a475130a63
Lethiwe Mchunu <mchunulethiwe6@gmail.com>
2022-05-27 03:11:22 +0200

    bug fix on utils

--- Utils.js
+++ Utils.js
@@ -1,80 +1,39 @@
 var io;
 var gameSocket;
-
-/**
- * This function is called by index.js to initialize a new game instance.
- *
- * @param sio The Socket.IO library
- * @param socket The socket object for the connected client.
- */
+// This function is called by index.js to initialize a new game instance.
 exports.initGame = function(sio, socket){
     io = sio;
     gameSocket = socket;
     gameSocket.emit('connected', { message: "You are connected!" });
-
     // Host Events
     gameSocket.on('hostCreateNewGame', hostCreateNewGame);
-
     // Player Events
     gameSocket.on('playerJoinGame', playerJoinGame);
 }
-
-/* *******************************
-   *                             *
-   *       HOST FUNCTIONS        *
-   *                             *
-   ******************************* */
-
-/**
- * The 'START' button was clicked and 'hostCreateNewGame' event occurred.
- */
+//The 'Create newGame' button was clicked and 'hostCreateNewGame' event occurred.
 function hostCreateNewGame() {
     // Create a unique Socket.IO Room
     var thisGameId = ( Math.random() * 100000 ) | 0;
-
     // Return the Room ID (gameId) and the socket ID (mySocketId) to the browser client
     this.emit('newGameCreated', {gameId: thisGameId, mySocketId: this.id});
-
     // Join the Room and wait for the players
     this.join(thisGameId.toString());
 };
-
-/* *****************************
-   *                           *
-   *     PLAYER FUNCTIONS      *
-   *                           *
-   ***************************** */
-
-/**
- * A player clicked the 'START GAME' button.////////////// susa
- * Attempt to connect them to the room that matches
- * the gameId entered by the player.
- * @param data Contains data entered via player's input - playerName and gameId.
- */
+//join button clicked
 function playerJoinGame(data) {
-    //console.log('Player ' + data.playerName + 'attempting to join game: ' + data.gameId );
-
     // A reference to the player's Socket.IO socket object
     var sock = this;
-
     // Look up the room ID in the Socket.IO manager object.
     var room = gameSocket.manager.rooms["/" + data.gameId];
-
     // If the room exists...
     if( room != undefined ){
         // attach the socket id to the data object.
         data.mySocketId = sock.id;
-
         // Join the room
         sock.join(data.gameId);
-
-        //console.log('Player ' + data.playerName + ' joining game: ' + data.gameId );
-
         // Emit an event notifying the clients that the player has joined the room.
         io.sockets.in(data.gameId).emit('playerJoinedRoom', data);
-
     } else {
-        // Otherwise, send an error message back to the player.
         this.emit('error',{message: "This room does not exist."} );
     }
 }
